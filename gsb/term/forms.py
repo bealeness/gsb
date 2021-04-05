a7208528073a0111165557c1e29ae81800a92ef5
Beale Stainton <bealeness@gmail.com>
2021-04-06 08:14:40 +1200

    Max-height on windows and term product options added

--- gsb/term/forms.py
+++ gsb/term/forms.py
@@ -1,12 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, DecimalField
+from wtforms import SubmitField, DecimalField, RadioField
 from wtforms.validators import DataRequired
 
-
+#choices need to be added as admin creates terms
 class TermProducts(FlaskForm):
+    product = RadioField('Term Product', choices=['The GSB Daily'], validators=[DataRequired()])
     amount = DecimalField('Enter the amount you wish to deposit:', validators=[DataRequired()], places=2)
     submit = SubmitField('Deposit')
 
+#choices need to be added as admin creates terms
 class WithdrawTerm(FlaskForm):
+    product = RadioField('Term Product', choices=['The GSB Daily'], validators=[DataRequired()])
     amount = DecimalField('Enter the amount you wish to withdraw:', validators=[DataRequired()], places=2)
     submit = SubmitField('Withdraw')
